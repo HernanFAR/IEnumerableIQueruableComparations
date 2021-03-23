5639059a926d055485b5aea27ade31a4da5b12ea
Hernan Alvarez <halvarez@Austranet.com>
2021-03-23 16:15:38 -0300

    Agregar archivos de proyecto.

---
+++ IEnumerable vs IQueryable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IEnumerable_vs_IQueryable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
